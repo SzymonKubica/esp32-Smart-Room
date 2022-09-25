6af9a2fcbf09fca64843d6f2095094127b32dbf3
Szymon Kubica <szymo.kubica@gmail.com>
2022-09-25 14:24:47 +0200

    Added a function to allow for connecting to the network.

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import machine
+import network
+import time
 from machine import Pin, SoftI2C
 from lcd_api import LcdApi
 from i2c_lcd import I2cLcd
 from time import sleep
 
+# Initialising the LCD display.
 I2C_ADDR = 0x27
 totalRows = 2
 totalColumns = 16
@@ -11,6 +14,20 @@ totalColumns = 16
 i2c = SoftI2C(scl=Pin(19), sda=Pin(23), freq=10000)
 lcd = I2cLcd(i2c, I2C_ADDR, totalRows, totalColumns)
 
+
+def do_connect():
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    if not wlan.isconnected():
+        print('connecting to network...')
+        wlan.connect('RK', 'rjsk62930')
+        while not wlan.isconnected():
+            pass
+    lcd.putstr('network config:', wlan.ifconfig())
+
+do_connect()
+
+time.sleep(10)
 while True:
     lcd.putstr("Hello Szymon")
     sleep(2)
